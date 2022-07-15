cd6abe924de8bcdb9212386d0cc37c5057d182fd
Luis Leonardo <llrwork1@gmail.com>
2022-07-14 19:37:12 -0500

    Change square divs from a class to an id

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const mainDiv = document.querySelector("#mainDiv");
 
 for (let i = 0; i <= 255; i++){
     let div = document.createElement("div");
-    div.classList.add("squareDivs"); 
+    //div.classList.add("squareDivs");
+    div.id = "squareDivs"; 
     mainDiv.appendChild(div);
 };
+
+const squareDivs = document.querySelector(".squareDivs");
+
+mainDiv.addEventListener("mouseover", () => {
+    squareDivs.classList.add("colorHover");
+})
