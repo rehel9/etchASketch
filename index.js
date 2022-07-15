7a00835832130ed50f5154c4ae10bf340e8fc540
Luis Leonardo <llrwork1@gmail.com>
2022-07-14 19:42:55 -0500

    Add selector, and change event for squareDivs

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const mainDiv = document.querySelector("#mainDiv");
 
 for (let i = 0; i <= 255; i++){
     let div = document.createElement("div");
-    //div.classList.add("squareDivs");
     div.id = "squareDivs"; 
     mainDiv.appendChild(div);
 };
 
-const squareDivs = document.querySelector(".squareDivs");
+const squareDivs = document.querySelector("#squareDivs");
 
-mainDiv.addEventListener("mouseover", () => {
+squareDivs.addEventListener("mousemove", () => {
     squareDivs.classList.add("colorHover");
 })
