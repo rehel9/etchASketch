858e5e7f6210110716825c18cb28e038bd0e827a
Luis Leonardo <llrwork1@gmail.com>
2022-07-14 19:09:26 -0500

    Fix quantity of divs and add styling to body and SD

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mainDiv = document.querySelector("#mainDiv");
 
-for (let i = 0; i <= 256; i++){
+for (let i = 0; i <= 255; i++){
     let div = document.createElement("div");
     div.classList.add("squareDivs"); 
     mainDiv.appendChild(div);
