4107d6b625be403f70fe0568623833477bfd8e0b
Luis Leonardo <llrwork1@gmail.com>
2022-07-17 05:22:35 -0500

    Modifie first for cycle to a function, and add button functionality to make new grid

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const sketchDiv = document.querySelector("#sketchDiv");
 
-for (let i = 0; i <= 255; i++){
-    let diver = document.createElement("diver");
-    diver.classList.add("squareDivs"); 
-    sketchDiv.appendChild(diver);
-};
+function createGrid(leftSideNumber, rightSideNumber){
+    let squares = (leftSideNumber * rightSideNumber) - 1;
+    for (let i = 0; i <= squares; i++){
+        let diver = document.createElement("div");
+        diver.classList.add("squareDivs"); 
+        sketchDiv.appendChild(diver);
+    };
+}
 
+createGrid(16, 16);
+
+const boton = document.querySelector("#boton");
 const squareDivs = document.querySelectorAll(".squareDivs");
 
-squareDivs.forEach((div) => {
+boton.addEventListener("click", () => {
+    let leftSideNumber = parseInt(prompt("How many squares for left side?", "16"));
+    let rightSideNumber = parseInt(prompt("How many squares for right side?", "16"));
+    while (sketchDiv.lastElementChild) {
+        sketchDiv.removeChild(sketchDiv.lastElementChild);
+    }
+    createGrid(leftSideNumber, rightSideNumber);
+});    
 
+squareDivs.forEach((div) => {
     div.addEventListener("mouseover", () => {
         div.classList.add("colorHover");
         })
